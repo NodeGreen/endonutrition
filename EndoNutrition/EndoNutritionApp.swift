defb6046e650818f9d757367812d50e9d707b4d7
NodeGreen <andrea.prest.endo@gmail.com>
2025-04-12 13:41:18 +0200

    Initial Commit

---
+++ EndoNutrition/EndoNutritionApp.swift
@@ -0,0 +1,17 @@
+//
+//  EndoNutritionApp.swift
+//  EndoNutrition
+//
+//  Created by Endo on 12/04/25.
+//
+
+import SwiftUI
+
+@main
+struct EndoNutritionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
